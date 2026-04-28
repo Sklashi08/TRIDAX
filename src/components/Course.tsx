6a2ff79c52e257e74db2e37d2df01d6fa90d4df9
unknown <sklasho08@gmail.comgit config --global user.name>
2026-04-28 10:41:26 +0100

    course we offer

---
+++ src/components/Course.tsx
@@ -0,0 +1,47 @@
+import "./Course.css";
+
+const Course = () => {
+  return (
+    <div>
+      <section className="course">
+        <h1>Course we offer</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit..</p>
+
+        <div className="row">
+          <div className="course-col">
+            <h3>Intermediate</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              beatae aspernatur placeat, unde earum iste fugit deserunt, sit
+              ipsum eveniet, explicabo eligendi accusantium neque consequuntur
+              laboriosam doloribus asperiores temporibus aliquid.
+            </p>
+          </div>
+
+          <div className="course-col">
+            <h3>Degree</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              beatae aspernatur placeat, unde earum iste fugit deserunt, sit
+              ipsum eveniet, explicabo eligendi accusantium neque consequuntur
+              laboriosam doloribus asperiores temporibus aliquid.
+            </p>
+          </div>
+
+          <div className="course-col">
+            <h3>Post Graduation</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              beatae aspernatur placeat, unde earum iste fugit deserunt, sit
+              ipsum eveniet, explicabo eligendi accusantium neque consequuntur
+              laboriosam doloribus asperiores temporibus aliquid.
+            </p>
+          </div>
+        </div>
+      </section>
+      ;
+    </div>
+  );
+};
+
+export default Course;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <p className="Albert">my name is Emepuru Albert Somkelechukwu</p>
+    </div>
+  );
+};
+
+export default Navbar;
